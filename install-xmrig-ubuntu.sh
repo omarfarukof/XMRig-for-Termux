4bac027555ebef6e5d72cb5559645fcaaf0cac79
Omar Faruk <omarfaruk20@iut-dhaka.edu>
2022-06-18 15:03:45 +0600

    install on ubuntu

---
+++ install-xmrig-ubuntu.sh
@@ -0,0 +1,43 @@
+
+apt update -y
+apt upgrade -y
+
+apt install git cmake build-essential libuv1-dev libssl-dev libhwloc-dev -y
+cd 
+git clone https://github.com/xmrig/xmrig.git
+cd xmrig
+
+mkdir build
+cd build
+xmr-dir=$(pwd)
+
+cmake -DWITH_HWLOC=OFF ..
+
+make
+xmrdir=$(pwd)
+
+cat >> $PATH/xmrig<< EOF
+#!$PATH/bash
+
+# Start XMRig in Termux
+
+case "$1" in
+        start)
+        .$xmrdir/xmrig -c
+        ;;
+        *)
+        echo "Wrong peramiter"
+        ;;
+esac
+
+EOF
+
+chmod +x $xmrdir/xmrig
+chmod +x $PATH/xmrig
+
+#
+echo "Go to \"https://xmrig.com/wizard\"  and  create your config.json settings"
+echo "place your config.json file in /xmrig/build"
+echo "Type the following command to start minimg =  \"./xmrig -c\"  or  \"xmrig start\""
+
+
